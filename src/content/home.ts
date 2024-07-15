34a410c1058c13b4e602e09554773f3ecee4677d
Anand Rajaram <anandrajaram21@gmail.com>
2024-07-15 11:01:16 +0530

    feat: content collection base created

--- src/content/home.ts
+++
@@ -1,8 +0,0 @@
-export const details = {
-  pageTitle: "Anand Rajaram | Home",
-  name: "Anand",
-  fullName: "Anand Rajaram",
-  // If you want multiple paragraphs, use \n\n between them. Keep it in a single line.
-  about: `I'm an enthusiastic Computer Software Architect 👨🏻‍💻 currently pursuing my Bachelors in Computer Science 🎓. I love exploring new technologies everyday and never pass up on an opportunity to learn 📱. I'm also immensely passionate about guiding and mentoring others who are new to the field 👨‍🏫. If there is one motto I live by, it's "Ship Good Code" 🚢 :)\n\nI have also had the privilege of working with Fortune 500 companies like Facebook (now Meta) and Cisco, all while adhering to strict rules, and developing clean, extensible, scalable, and robust codebases. As a result, writing clean code has been engrained into my workflow, and collaborating with teammates across different time zones has become second nature ;)`,
-  links: [],
-};
